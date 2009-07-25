b8fe888552d811d31332dedf6381867a7ea83a8d
kirchrath <kirchrath@6e482caa-7914-11de-9d9e-e13182ece997>
2009-07-25 13:11:11 +0000

    Solver jetzt performant und richtig - bugfree

--- trunk/Shisen/src/gdi1shisen/gamecontroller/Solver.java
+++ trunk/Shisen/src/gdi1shisen/gamecontroller/Solver.java
@@ -4,7 +4,6 @@ import java.util.LinkedList;
 
 import gdi1shisen.datastore.Brick;
 import gdi1shisen.datastore.LevelParser;
-import gdi1shisen.datastore.Point;
 import gdi1shisen.exceptions.ParameterOutOfRangeException;
 import gdi1shisen.exceptions.SyntacticIncException;
 
@@ -13,15 +12,12 @@ import gdi1shisen.exceptions.SyntacticIncException;
  * @author chandra
  *
  */
-public class Solver 
+public class Solver
 {
 	private LevelParser lpObject;
 	private LinkedList<int[][]> loesungsWeg = new LinkedList<int[][]>();
-	
-
+	private int counter = 0;
 	private boolean solvable = false;
-	private LinkedList<LevelParser> lpHistory = new LinkedList<LevelParser>();
-	
 	
 	/**
 	 * Initialisiert das Object und überprüft das level auf Lösbarkeit und 
@@ -34,58 +30,52 @@ public class Solver
 	throws ParameterOutOfRangeException, SyntacticIncException
 	{
 		this.lpObject =lpObject.clone();
-		solverLogic();
-		
+		solverLogic(this.lpObject);
 	}
 	
-	
-	
-	
-	public void solverLogic() throws ParameterOutOfRangeException, SyntacticIncException{
-		boolean couldBeSolved = true;
-		LevelParser lpObject = this.lpObject.clone();
-		LinkedList<int[][]> apm;
-		int positionToUse = 0;
-		int rised = 1;
-		int ebene = 1;
-		while(!solvable || couldBeSolved)
-		{
-			apm = getActualPossibleMoves(lpObject.clone());
-			if(apm.size()>0){
-				if(positionToUse>apm.size())
-				{
-					couldBeSolved = false;
-					break;
-				}
-				ebene++;
-				LevelParser genL = easyMove(lpObject,apm.get(positionToUse));
-				lpHistory.add(genL.clone());
-				if(Move.isSolved(genL))
+	/**
+	 * Sucht nach einem Lösungsweg nach dem Prinzip der Tiefensuche und speichert diesen in loesungsWeg ab
+	 * @param lpObject	Das zu lösende Level
+	 * @throws ParameterOutOfRangeException
+	 * @throws SyntacticIncException
+	 */
+	private void solverLogic(LevelParser lpObject) 
+	throws ParameterOutOfRangeException, SyntacticIncException
+	{
+		if(!solvable){
+			LinkedList<int[][]> apm = getActualPossibleMoves(lpObject);
+			for(int i=0;i<apm.size();i++)
+			{
+				if(solvable) break;
+				LevelParser lp = easyMove(lpObject,apm.get(i));
+				loesungsWeg.add(counter, apm.get(i));
+				counter++;
+				if(Move.isSolved(lp))
 				{
 					solvable = true;
-					break;
 				}else
 				{
-					lpObject = genL.clone();
+					solverLogic(lp);
 				}
-				if(positionToUse>0) {
-					positionToUse--;
+				if(!solvable)
+				{
+					loesungsWeg.removeLast();
+					counter--;
 				}
-			}else{
-				ebene--;
-				rised++;
-				lpHistory.removeLast();
-				LevelParser lpTMPObject = lpHistory.removeLast();
-				lpObject = lpTMPObject.clone();
-				positionToUse++;
 			}
 		}
-		setSol(this.lpObject.clone(),lpHistory);
 	}
 
-
-
-	private LevelParser easyMove(LevelParser lpObject,int[][] move) throws SyntacticIncException{
+	/**
+	 * löscht zwei steine aus einem LevelParser-Object und liefert ein neues verändertes zurück
+	 * @param lpObject	Das LevelParser-Object
+	 * @param move		koordinaten der zwei punkte in int[][]{{x0,y0},{x1,y1}}
+	 * @return			das neue LevelParser-Object
+	 * @throws SyntacticIncException
+	 */
+	private LevelParser easyMove(LevelParser lpObject,int[][] move) 
+	throws SyntacticIncException
+	{
 		LevelParser lp = lpObject.clone();
 		char[][] level = lp.getRawLevel();
 		level[move[0][1]-1][move[0][0]-1] = '-';
@@ -93,145 +83,12 @@ public class Solver
 		lp = new LevelParser(level);
 		return lp;
 	}
-	
-	@SuppressWarnings("unchecked")
-	private void setSol(LevelParser lpObject,LinkedList<LevelParser> lpHistory) throws ParameterOutOfRangeException, SyntacticIncException{
-		lpHistory = (LinkedList<LevelParser>) lpHistory.clone();
-		LevelParser lp1 = lpObject;
-		LevelParser lp2 = lpHistory.removeFirst().clone();
-		while(lpHistory.size()>0){
-			int difC = difCounter(lp1,lp2);
-			if(difC==2){
-				loesungsWeg.addLast(getMoveNormal(lp1,lp2));
-			}else{
-				LinkedList<int[][]> nn = getMultipleMove(lp1,lp2);
-				while(nn.size()>0)
-				{
-					loesungsWeg.add(nn.removeFirst());
-				}
-			}
-			lp1 = lp2.clone();
-			lp2 = lpHistory.removeFirst().clone();
-			if(lpHistory.size()==0){
-				difC = difCounter(lp1,lp2);
-				if(difC==2){
-					loesungsWeg.addLast(getMoveNormal(lp1,lp2));
-				}else{
-					LinkedList<int[][]> nn = getMultipleMove(lp1,lp2);
-					while(nn.size()>0)
-					{
-						loesungsWeg.add(nn.removeFirst());
-					}
-				}
-				
-			}
-		}
-	}
-	
-	
-	private int difCounter(LevelParser lp1, LevelParser lp2){
-		char[] lp1C = lp1.toString().toCharArray();
-		char[] lp2C = lp2.toString().toCharArray();
-		LinkedList<Integer> pids = new LinkedList<Integer>();
-		int difCounter=0;
-		for(int i=0;i<lp1C.length;i++){
-			if(lp1C[i]!=lp2C[i]) difCounter++;
-			pids.add(i);
-		}
-		return difCounter;
-	}
-	
-	private int[][] getMoveNormal(LevelParser lp1, LevelParser lp2) throws ParameterOutOfRangeException{
-		char[] lp1C = lp1.toString().toCharArray();
-		char[] lp2C = lp2.toString().toCharArray();
-		int pid1 = -1;
-		int pid2 = -1;
-		for(int i=0;i<lp1C.length;i++){
-			if(lp1C[i]!=lp2C[i]){
-				if(pid1==-1){
-					pid1 = i;
-				}else{
-					pid2 = i;
-				}
-			}
-		}
-		int[] pCoord1 = Point.mkCoord(pid1, lp1.getRawLevel());
-		int[] pCoord2 = Point.mkCoord(pid2, lp2.getRawLevel());
-		int[][] output = new int[2][2];
-		output[0] = new int[]{pCoord1[0]+1,pCoord1[1]+1};
-		output[1] = new int[]{pCoord2[0]+1,pCoord2[1]+1};
-		return output;
-	}
-	
-	
-	@SuppressWarnings("unchecked")
-	private LinkedList<int[][]> getMultipleMove(LevelParser lp1, LevelParser lp2) throws ParameterOutOfRangeException, SyntacticIncException{
-		LinkedList<int[][]> innerSol = new LinkedList<int[][]>();
-		LinkedList<Integer> pidsTwo = new LinkedList<Integer>();
-		
-		char[] lp1C = lp1.toString().toCharArray();
-		char[] lp2C = lp2.toString().toCharArray();
-		
-		for(int i=0;i<lp1C.length;i++){
-			if(lp1C[i]!=lp2C[i]){
-				pidsTwo.add(i);
-			}
-		}
 
-		
-		LinkedList<Integer> pClone1 = (LinkedList<Integer>) pidsTwo.clone();
-		LevelParser lClone = lp1.clone();
-		while(pidsTwo.size()>0){
-			for(int i=0;i<pClone1.size();i++){
-				for(int k=0;k<pClone1.size();k++){
-					if(k!=i){
-						int[] punktA = refakt(pClone1.get(i),lClone);
-						int[] punktB = refakt(pClone1.get(k),lClone);
-						if(Move.isWay(lClone, punktA, punktB)){
-							innerSol.add(new int[][]{punktA,punktB});
-							LevelParser  lClone2 = lClone.clone();
-							lClone = easyMove(lClone2,new int[][]{punktA,punktB});
-							LinkedList<Integer> tmpList = new LinkedList<Integer>();
-							tmpList.add(pClone1.get(i));
-							tmpList.add(pClone1.get(k));
-							pClone1.removeAll(tmpList);
-						}
-					}
-				}
-			}
-			pidsTwo = pClone1;
-		}
-		return innerSol;
-	}
-	
-
-	public int[] refakt(int id,LevelParser lp) throws ParameterOutOfRangeException{
-		int[] p = Point.mkCoord(id, lp.getRawLevel());
-		int[] out = new int[2];
-		out[0] = p[0]+1;
-		out[1] = p[1]+1;
-		return out;
-	}
-	
-	public String getCode(int[][] i){
-		StringBuffer sb = new StringBuffer();
-		sb.append(i[0][1]);
-		sb.append(":");
-		sb.append(i[0][0]);
-		sb.append(" - ");
-		sb.append(i[1][1]);
-		sb.append(":");
-		sb.append(i[1][0]);
-		return sb.toString();
-	}
-	
-	
-	
 	
 	/**
 	 * Welche Züge sind aktuell möglich?
 	 * @param lpObject	Das Level
-	 * @return	die Liste der aktuell möglichen Züge
+	 * @return			die Liste der aktuell möglichen Züge
 	 * @throws ParameterOutOfRangeException
 	 */
 	private LinkedList<int[][]> getActualPossibleMoves(LevelParser lpObject) 
